b95e9192f53afe7c60ca8e07f5da0f327123bbda
Derek Marion <github.magnify953@passmail.net>
2024-12-09 15:11:03 -0800

    Add some nodes to the simple undirected graph

--- src/graphs.py
+++ src/graphs.py
@@ -1,6 +1,6 @@
 import networkx as nx
 
-# Simple graph
+# Simple undirected graph
 simple_graph = nx.Graph()
 simple_graph.add_edges_from(
     [
@@ -12,18 +12,42 @@ simple_graph.add_edges_from(
         ("D", "G"),
         ("E", "H"),
         ("F", "I"),
+        ("I", "J"),
+        ("I", "B"),
+        ("E", "L"),
     ]
 )
 
-# Binary tree graph
+# Binary tree with numbers as nodes
 binary_tree = nx.Graph()
 binary_tree.add_edges_from(
     [
-        ("A", "B"),
-        ("A", "C"),
-        ("B", "D"),
-        ("B", "E"),
-        ("C", "F"),
-        ("C", "G"),
+        (1, 2),
+        (1, 3),
+        (2, 4),
+        (2, 5),
+        (3, 6),
+        (3, 7),
+        (4, 8),
+        (4, 9),
+        (5, 10),
+        (5, 11),
+        (6, 12),
+        (6, 13),
+    ]
+)
+
+# Graph with weighted edges (for Dijkstra's algorithm)
+weighted_graph = nx.Graph()
+weighted_graph.add_weighted_edges_from(
+    [
+        ("A", "B", 1),
+        ("A", "C", 3),
+        ("B", "D", 2),
+        ("B", "E", 4),
+        ("C", "F", 5),
+        ("D", "G", 6),
+        ("E", "H", 7),
+        ("F", "I", 8),
     ]
 )
